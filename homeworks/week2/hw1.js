17e466628a563d21471f9d6370ddb178c7fc8922
Adam Yen <adamyen214@hotmail.com>
2020-06-27 14:48:14 +0800

    hw2 done

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,16 @@
 function printStars(n) {
-  
+  var answer=''
+  for(var i=1;i<=n;i++){
+    answer+='*\n'
+  }
+  console.log(answer)
 }
 
-printStars(5);
+function foo(n) {
+  for(let i = 0; i < n; i++) {
+    console.log('*')
+  }
+}
+
+// printStars(5)
+foo(3)
--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,8 @@
 function capitalize(str) {
-  
+  var firstUppercaseLetter = str.charAt(0).toUpperCase()+str.slice(1)
+  return firstUppercaseLetter
 }
 
-console.log(capitalize('hello'));
+console.log(capitalize('nick'));
+console.log(capitalize('Nick'));
+console.log(capitalize(',hello'));
