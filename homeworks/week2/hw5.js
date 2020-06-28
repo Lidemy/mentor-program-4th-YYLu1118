6e32f6c061fa70d0243ff78c460d8f9536896cba
Adam Yen <adamyen214@hotmail.com>
2020-06-28 17:59:43 +0800

    hw5 done

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,27 @@
 function join(arr, concatStr) {
-  
+  let joinedStr = ''
+  for (let i=0; i<arr.length; i++) {
+      if (i !== arr.length-1) {
+          joinedStr += arr[i]
+          joinedStr += concatStr
+      } else {
+          joinedStr += arr[i]
+      } 
+  }
+  return joinedStr
 }
 
 function repeat(str, times) {
-  
+  let repeatedStr = ''
+  for ( let i = 0; i < times; i++){
+    repeatedStr += str
+  }
+  return repeatedStr
 }
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(join(["a", "b", "c"], "!"))
+console.log(join(['a', 1, 'b', 2, 'c', 3], ','))
+console.log(repeat('a', 5))
+console.log(repeat('yoyo', 2))
+
+
